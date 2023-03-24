05ae04521e095c99b17bcee2cbcac5f20afa7e46
RensKooijman <113177921+RensKooijman@users.noreply.github.com>
2023-03-24 10:04:27 +0100

    niet meer klikken tijdens animatie en data komt na de roll

--- dice.js
+++ dice.js
@@ -1,5 +1,5 @@
 let allValues = [];
-let degrees = [[0, 60, -45], [45, -20, 70], [60, 165, 20], [60, 165, 200], [45, -20, 250], [0, 240, -45]];
+let degrees = [[0, 60, -45], [45, -20, 70], [50, 160, 20], [50, 160, 200], [45, -20, 250], [0, 240, -45]];
 //geeft het random getal en zorgt dat dat aantal in de dice komt 
 let roll = document.querySelector(".dice");
 roll.addEventListener("click", () =>{
@@ -8,26 +8,27 @@ roll.addEventListener("click", () =>{
   const average = document.getElementById("average");
   const list = document.getElementById("list");
 
-  console.log(randomNumber);
-  allValues.push(randomNumber);
-  average.innerHTML = getAverage(allValues);
-  list.innerHTML='';
-  //zorgt ervoor dat het aantal keer van getal wat gerold word in lijst komt
-  const counts = {};
-  for (let i = 0; i < allValues.length; i++) {
-    const number = allValues[i];
-    counts[number] = counts[number] ? counts[number] + 1 : 1;
-  }
-  for (const number in counts) {
-    const count = counts[number];
-    const listItem = document.createElement("li");
-    listItem.innerHTML = `${number}: ${count}`;
-    list.appendChild(listItem);
-  }
-  document.body.appendChild(list);
   setTimeout(function() {
     roll.classList.remove("rotate");
     roll.style.transform = `rotateY(${degrees[randomNumber-1][0]}deg) rotateX(${degrees[randomNumber-1][1]}deg) rotateZ(${degrees[randomNumber-1][2]}deg)`;
+
+    console.log(randomNumber);
+    allValues.push(randomNumber);
+    average.innerHTML = getAverage(allValues);
+    list.innerHTML='';
+    //zorgt ervoor dat het aantal keer van getal wat gerold word in lijst komt
+    const counts = {};
+    for (let i = 0; i < allValues.length; i++) {
+      const number = allValues[i];
+      counts[number] = counts[number] ? counts[number] + 1 : 1;
+    }
+    for (const number in counts) {
+      const count = counts[number];
+      const listItem = document.createElement("li");
+      listItem.innerHTML = `${number}: ${count}`;
+      list.appendChild(listItem);
+    }
+    document.body.appendChild(list);
   }, 2500);
 });
 //berekend het gemmiddelde
